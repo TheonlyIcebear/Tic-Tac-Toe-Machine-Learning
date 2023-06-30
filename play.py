6d4830e0bd79e839b4411ce19bf7a63515f4c498
TheonlyIcebear <78031685+TheonlyIcebear@users.noreply.github.com>
2023-06-29 22:27:02 -0400

    Add files via upload

--- play.py
+++ play.py
@@ -1,71 +1,36 @@
 from multiprocessing import Process, Queue
-from numba import jit, cuda
+from utils.model import Model
 from utils.game import Game
+from numba import jit, cuda
 from tqdm import tqdm
 import threading, hashlib, random, numpy as np, math, copy, time, json
-            
-class Model:
-    def __init__(self, model):
-        self.model = model
-        self._layer_outputs = []
-        self._previous_changes = np.array([])
-        np.seterr(all="ignore")
-
-    # The activation function
-    def _sigmoid(self, x):
-        if x > 100:
-            return 1
-
-        return 1 / ( 1 + np.exp(-x) )
-
-    # Run the model to get a output
-    @jit(forceobj=True)
-    def eval(self, input):
-        answer = 0
-        model = self.model
-
-        previous_layer_output = input
-
-        self._layer_outputs = np.array([])
-
-        for count, layer in enumerate(model):
-
-            layer_output = np.array([])
- 
-            for node in layer:
-                weights = node[:len(previous_layer_output)]
-                bias = node[-1]
-
-                output = self._sigmoid(np.dot(weights, previous_layer_output) + bias)
-
-                layer_output = np.append(layer_output, output)
-
-
-            if self._layer_outputs.size:
-                self._layer_outputs = np.vstack([self._layer_outputs, layer_output])
-            else:
-                self._layer_outputs = np.hstack([self._layer_outputs, layer_output])
-
-            previous_layer_output = np.array(layer_output)
-
-        return layer_output[:9]
 
 class Main:
     def __init__(self):
-        self.brain = json.load(open('model-training-data.py', 'r+'))
+        file = json.load(open('model-training-data.py', 'r+'))
+
+        self.brain = [np.array(data) for data in file[:2]] + file[2:]
         self.play()
 
     def render(self, grid):
         print('\n'.join([''.join([str(tile) for tile in row]) for row in grid]).replace('0', '-').replace('1', 'x').replace('2', 'o'))
 
     def play(self):
+        model, heights, hidden_activation_function, output_activation_function, cost_function = self.brain
+        model = Model(
+            model=model,
+            heights=heights,
+            hidden_function = hidden_activation_function,
+            output_function = output_activation_function,
+            cost_function=cost_function
+        )
+
         while True:
             game = Game()
             self.render(game.grid)
             points = 0
             while True:
-
-                model = Model(self.brain)
+                
                 if (game.grid == 0).all():
                     player = random.randint(0, 1)
 
@@ -92,19 +57,50 @@ class Main:
                     
                 else:
                 
-                    grid = np.array(game.grid.flatten())
-                    open_slots = game.open_slots
+                    grid = game.grid.flatten()
+                    # rotation = game.normalization_rotation_offset
+                    # grid = np.rot90(grid, rotation).flatten()
 
-                    real_grid = grid
-                    real_grid[grid == 2] = -1
+                    open_slots = game.open_slots.tolist()
 
-                    prediction = model.eval(real_grid)
-                    largest_value = np.max(prediction[open_slots])
-                    choice = np.where(prediction == largest_value)[0][0]
+                    real_grid = np.zeros(18)
+                    real_grid[:9][grid == player + 1] = 1
+                    real_grid[9:][grid == (1 - player) + 1] = 1
 
-                    print(prediction, game.best_moves(1))
+                    model_outputs = model.eval(
+                        real_grid
+                    )
+
+                    model_outputs = [np.array(array) for array in model_outputs]
+
+                    print(model_outputs[-1])
+
+                    raw_prediction = np.array(model_outputs[-1])
+
+                    mask = np.ones(raw_prediction.shape, bool)
+                    mask[open_slots] = False
+
+                    prediction = np.array(raw_prediction)
+                    prediction[mask] = -1
+
+                    choice = np.argmax(prediction)
+
+                    best_moves = game.best_moves(1)
 
                     evaluation = game.select(choice, 1)
+
+                    expected_output = np.array([0] * 9)
+                    expected_output[best_moves] = 1
+
+                    gradient, average_cost = model.gradient(
+                        model_outputs, 
+                        expected_output
+                    )
+
+                    print(real_grid)
+                    print(raw_prediction, choice)
+                    print(expected_output, best_moves)
+                    print(average_cost)
 
                     print("+++ Bot's Turn")
 
